1439832e74f2041e28f23abd1bd7f37a8e60968b
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2022-03-13 00:32:43 +0530

    backend without db

---
+++ iwpBlogBackend/about.php
@@ -0,0 +1,38 @@
+<?php include("path.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about us</title>
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+    <p style="color: black; font-size: 30px; text-align: center; width: 80%; margin: auto; padding: 40px;">Blogging Website made as the J component project for CSE3002, Internet and Web Programming, using PHP and MySQLi.</p>
+    <p style="color: black; font-size: 20px; text-align: center;">Group: <br></p>
+    <p style="color: black; font-size: 15px; text-align: center;">Apoorva Srivastava (19BCE2138) <br></p>
+    <p style="color: black; font-size: 15px; text-align: center;">Sudhanshu Agarwal (19BCE2203) <br></p>
+    <p style="color: black; font-size: 15px; text-align: center; padding-bottom:60px;">Namrata Singhal (19BCE0643) <br></p>
+    <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+</body>
+</html>
---
+++ iwpBlogBackend/mail.php
@@ -0,0 +1,15 @@
+<?php
+//get data from form  
+if(isset($_POST['contact-submit'])){
+    $email= $_POST['contactEmail'];
+    $message= $_POST['contactMessage'];
+    $to = "apoorvasrivastava.14@gmail.com";
+    $subject = "Mail From BlogSquare";
+    $txt ="\r\n  Email = " . $email . "\r\n Message =" . $message;
+    $headers = "From: noreply@blogsquare.com" . "\r\n" ."CC: somebodyelse@example.com";
+    if($email!=NULL){
+        mail($to,$subject,$txt,$headers);
+    }
+}
+
+?>
